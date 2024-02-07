4a06327bd3e3b3ab028f469ca06c6bcce65f736f
rizzpain <158843178+rizzpain@users.noreply.github.com>
2024-02-07 22:48:14 +0530

    Create Valid-parenthesis.cpp

---
+++ Valid-parenthesis.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty()){
+                    if(s[i] == ')' && st.top() == '('){
+                        st.pop();
+                    }
+                    else if(s[i] == '}' && st.top() == '{'){
+                        st.pop();
+                    }
+                    else if(s[i] == ']' && st.top() == '[' ){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        if(st.empty()){
+            return true;
+        }
+
+        return false;
+    }
+};
